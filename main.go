5c11f81be0efd5d160222d7c33f4daada9525b49
shinta <flekystyley@gmail.com>
2024-07-20 13:30:39 +0900

    fix

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"os"
-
 	"github.com/runetale/runevision/api_router"
 	"github.com/runetale/runevision/di"
 	"github.com/runetale/runevision/domain/config"
-	"github.com/runetale/runevision/router"
-	"golang.org/x/exp/maps"
-	"golang.org/x/net/proxy"
 )
 
 func main() {
 	// api
 	cfg := config.MustLoad()
 	db := di.InitializePostgres(cfg.Postgres, cfg.Log)
-	err = db.MigrateUp("migrations")
+	err := db.MigrateUp("migrations")
 	if err != nil {
 		panic(err)
 	}
